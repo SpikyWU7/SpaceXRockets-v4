66ef773ba3abc2402d0c0ffe8aafb571fddcc330
SpikyWU7 <spikywu7@gmail.com>
2022-08-10 18:56:38 +0300

    settingsVC without segments

--- SpaceXRockets_v4/Controllers/SettingsViewController.swift
+++ SpaceXRockets_v4/Controllers/SettingsViewController.swift
@@ -10,27 +10,11 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet var segmentHeight: UISegmentedControl!
-    @IBOutlet var segmentDiameter: UISegmentedControl!
-    @IBOutlet var segmentMass: UISegmentedControl!
-    @IBOutlet var segmentPayload: UISegmentedControl!
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func heightChanged(_ sender: UISegmentedControl) {
-        UserDefaults.standard.set(sender.isSelected ? "kg" : "lb", forKey: "Mass")
+    @IBAction func dismissPressed(_ sender: UIButton) {
+        self.presentingViewController?.dismiss(animated: true)
     }
-
-    @IBAction func diameterChanged(_ sender: UISegmentedControl) {
-    }
-
-    @IBAction func massChanged(_ sender: UISegmentedControl) {
-    }
-
-    @IBAction func payloadChanged(_ sender: UISegmentedControl) {
-    }
-
 
 }
